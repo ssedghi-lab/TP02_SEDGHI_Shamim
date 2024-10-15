d8190eedd51712c67ee42815a3150ea1d049e32f
ssedghi <shamim.sedghi@etu.unistra.fr>
2024-10-15 19:08:52 +0200

    last change

--- FRONTEND/shared/shared.module.ts
+++ FRONTEND/shared/shared.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';  // Importe CommonModule, nécessaire pour les directives communes comme ngIf, ngFor, etc.
+import { CommonModule } from '@angular/common';  
 
 @NgModule({
   imports: [
-    CommonModule  // Importe les fonctionnalités de base d'Angular dans le module
+    CommonModule  
   ],
   declarations: [
-    // Liste des composants, directives, pipes que vous voulez déclarer dans ce module
   ],
   exports: [
-    // Liste des composants, directives, pipes que vous souhaitez réutiliser ailleurs dans l'application
   ]
 })
 export class SharedModule { }
